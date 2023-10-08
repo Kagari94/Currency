0d302576b6c43cd16036b63b6ee9079de87fee6c
Kagari94 <jesse.hirvo@hotmail.com>
2023-10-08 14:40:17 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import axios from 'axios' 
+
+const API = "KEY HERE"
+const URL = "https://api.freecurrencyapi.com/v1/latest?apikey="+API
 
 function App() {
+  const [eur, setEur] = useState(0);
+  const [gbp, setGbp] = useState(0);
+  const [rate, setRate] = useState(0);
+
+  const convert = (e) => {
+    e.preventDefault()
+    axios.get(URL).then((response) => {
+      const json = response.data
+      setRate(json.data.GBP)
+      setGbp(eur * json.data.GBP)
+
+    }).catch(error => {
+      alert(error)
+    })
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <form onSubmit={convert}>
+        <div>
+          <label>EUR</label>&nbsp;
+          <input type="number" step="0.01" value={eur} onChange={e => setEur(e.target.value)}/>
+          <output> {rate}</output> 
+        </div>
+        <div>
+          <label>Gbp</label>
+          <output> {gbp.toFixed(2)}</output>
+        </div>
+        <div>
+          <button>Calculate</button>
+        </div>
+      </form>
     </div>
   );
 }
